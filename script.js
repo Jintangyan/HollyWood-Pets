995ebfeba8d6da2072d5442bcaafcf83c0d9b240
Jintang Yan <9180@ait.nsw.edu.au>
2023-08-06 14:58:33 +1000

    reorginise

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // open and close navigation
 // button.addEventListener()
-function onload(evt) {
-    const button = document.querySelector('.nav-button')
-    const nav = document.querySelector('.navigation')
+// function onload(evt) {
+//     const button = document.querySelector('.nav-button')
+//     const nav = document.querySelector('.navigation')
 
-    const resetBt = document.querySelector('#reset')
-    const submitBt = document.querySelector('#submit')
-    button.addEventListener('click', function () {
-        if (nav.classList.contains('open')) {
-            nav.classList.remove('open')
-        }
-        else {
-            nav.classList.add('open')
-        }
-    })
+//     const resetBt = document.querySelector('#reset')
+//     const submitBt = document.querySelector('#submit')
+//     button.addEventListener('click', function () {
+//         if (nav.classList.contains('open')) {
+//             nav.classList.remove('open')
+//         }
+//         else {
+//             nav.classList.add('open')
+//         }
+//     })
 
-    if(resetBt){
-        resetBt.addEventListener('click', function () {
-            alert('reset button clicked');
-        })
-    }
-    if(submitBt){
-        submitBt.addEventListener('click', function () {
-            alert('soon I will create this');
-        })
-    }
-}
+//     if(resetBt){
+//         resetBt.addEventListener('click', function () {
+//             alert('reset button clicked');
+//         })
+//     }
+//     if(submitBt){
+//         submitBt.addEventListener('click', function () {
+//             alert('soon I will create this');
+//         })
+//     }
+// }
 
-window.addEventListener('load', onload)
+// window.addEventListener('load', onload)
